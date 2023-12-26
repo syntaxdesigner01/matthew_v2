76e705e1341d1fcf53237a3b728481b37b249ae8
syntaxdesigner01 <syntaxdesigners@gmail.com>
2023-12-26 07:38:11 +0000

    done reponsive design for nabar and header

--- src/components/HomeComponents/Header.tsx
+++ src/components/HomeComponents/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function Header() {
   return (
-    <div className="mt-[5em] bg-bgDark  flex justify-between px-20 pt-20 pb-10 gap-10 items-center">
+    <div className="mt-[5em] bg-bgDark flex flex-col  justify-center  px-4  lg:px-20 pt-20 pb-10 gap-10 lg:justify-between lg:items-center lg:flex-row">
 
         <div>
             <div className="absolute top-0 left-[20em]">
@@ -12,12 +12,13 @@ export default function Header() {
                 <Image src={'/bg-gradient.svg'} alt="bg-gradient"  width={300} height={200}/>
             </div>
 
-            <div className="">
-                <h1 className="text-4xl text-primary w-[90%] leading-relaxed tracking-wide">Taking Your Productivity To Maximum</h1>
-                <p className="text-white text-xl py-4 w-[70%] leading-relaxed tracking-wide">Focus more on creativity...let&apos;s handle productivity</p>
+            <div className=" flex flex-col w-full justify-center items-center lg:block">
+                <h1 className="text-2xl text-left lg:text-4xl text-primary w-[90%] leading-relaxed tracking-wide">Taking Your Productivity To Maximum</h1>
+
+                <p className="text-white lg:text-xl py-4 lg:w-[70%] leading-relaxed text-left px-4 lg:px-0 tracking-wide">Focus more on creativity...let&apos;s handle productivity</p>
             </div>
 
-            <div className="w-[80%] mt-10">
+            <div className="w-[80%] mt-10 hidden lg:block">
                 <button className="bg-primary py-2 rounded-md capitalize text-white w-full">Get started</button>
             </div>
         </div>
@@ -29,6 +30,9 @@ export default function Header() {
             <p className="text-white text-center py-4">Track <span className="text-primary">Productivity</span> </p>
         </div>
         
+        <div className="w-full mt-4 lg:hidden">
+                <button className="bg-primary py-2 rounded-md capitalize text-white w-full">Get started</button>
+            </div>
     </div>
   )
 }
