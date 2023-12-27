deb46d15649d11aeef28a8971c4d84813ea6af27
syntaxdesigner01 <syntaxdesigners@gmail.com>
2023-12-27 19:47:17 +0100

    added fonts worked on dashboard nav  layout

--- src/components/DashboardComponents/DashboardNav.tsx
+++ src/components/DashboardComponents/DashboardNav.tsx
@@ -4,10 +4,19 @@ import Image from "next/image";
 export default function DashboardNav() {
   return (
 
-        <nav className="bg-black flex justify-between px-20 py-4 ">
+        <nav className="bg-black flex justify-between items-center px-4 py-4 ">
+            <div className="border-b-2 border-primary pb-2 px-8">
             <Image src={'/logo.png'} alt="user" width={200} height={200} />
+            </div>
+           
+           <div  className="flex text-center gap-2 border border-primary p-2 rounded-tr-3xl rounded-bl-3xl text-sm">
+           <Image src={'/user.svg'} alt="user" width={50} height={50} />
 
-            <Image src={'/user.svg'} alt="user" width={50} height={50} />
+           <div className="text-white">
+            <p>Welcome</p>
+            <p>Rofownkelvin</p>
+           </div>
+           </div>
         </nav>
  
   )
---
+++ src/components/DashboardComponents/SideMenu.tsx
@@ -0,0 +1,7 @@
+export default function SideMenu() {
+  return (
+    <div className="shadow-md border-r-2 border-primary w-[10%] h-[100vh]">
+        <h1>hello</h1>
+    </div>
+  )
+}
