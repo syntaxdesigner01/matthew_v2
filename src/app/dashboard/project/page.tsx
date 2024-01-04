1072cb0ce02dd6d219d49fbc98dd65fbe2aed051
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-01-04 18:49:58 +0100

    manage app state

--- src/app/dashboard/project/page.tsx
+++ src/app/dashboard/project/page.tsx
@@ -1,34 +1,36 @@
 'use client'
 import CreateProject from '@/components/DashboardComponents/CreateProject';
 import NoProject from '@/components/DashboardComponents/NoProject';
-import { AppDispatch, RootState } from '@/store/store';
-import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/store/store';
+import { useEffect, useState } from 'react';
+import {useSelector } from 'react-redux'
 
 export default function Project() {
   const projects = useSelector((state: RootState) => state.projects)
-  const dispatch = useDispatch()
+  const [isProject, setIsProject] = useState<boolean>(false)
 
-  console.log(projects);
+
+  useEffect(() => {
+    if (projects.length !== 0) setIsProject(false)
+    else setIsProject(true)
+  }, [])
 
 
   return (
     <>
       <div className='flex w-full h-full justify-center items-center'>
-        <div className={`z-50 w-[40%] border shadow-xl p-10 ${projects.length !== 0 ? 'rounded-tr-[12%] rounded-bl-[12%] rounded-2xl' : 'rounded-tr-[17%] rounded-bl-[17%] rounded-lg'} flex flex-col gap-10`}>
+        <div className={`z-50 w-[40%] border shadow-xl p-10 ${!isProject ? 'rounded-tr-[12%] rounded-bl-[12%] rounded-2xl' : 'rounded-tr-[17%] rounded-bl-[17%] rounded-lg'} flex flex-col gap-10`}>
 
           {
-            projects.length === 0 ?
-
+            isProject ?
               <NoProject />
-
               :
-
               <CreateProject />
 
           }
         </div>
         <div>
-          <img src="/projectAvater.png" alt="" height={200} width={200} />
+          <img src="/projectAvater.png" alt="" height={250} width={200} />
         </div>
 
 
