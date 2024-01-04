e4d3d76469b9f26d12b562e5c25591f397a629a4
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-01-04 17:43:25 +0100

    done with the no project component

--- src/app/dashboard/project/page.tsx
+++ src/app/dashboard/project/page.tsx
@@ -8,9 +8,31 @@ export default function Project() {
 
   return (
     <>
-    {
-      projects.length === 0 ? <p>no project</p> : 'page'
-    }
+      {
+        projects.length === 0 ?
+          <div className='flex w-full h-full justify-center items-center'>
+            <div className='z-50 border shadow-xl p-10 rounded-tr-[10%] w- rounded-bl-[10%] rounded-md flex flex-col gap-10'>
+              <p className='text-center font-bold'>Sorry you have no Project at the moment. Please <br /> kindly create a New Project </p>
+
+              <div className='flex justify-center '>
+                <img src="/tasklist.png" alt="" height={150} width={150} />
+              </div>
+
+              <div className='flex justify-center items-center'>
+              <button className='bg-black w-full text-primary py-2 rounded-md capitalize'>
+                create New Project
+              </button>
+            </div>
+            </div>
+
+            <div>
+              <img src="/projectAvater.png" alt="" height={200} width={200} />
+            </div>
+
+
+          </div>
+          : 'page'
+      }
     </>
   )
 }
